7d546baa81b25b73570f13dccec3511873cd1901
NISAL <535964903@qq.com>
2018-06-12 11:13:45 +0800

    重构项目

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import uglify from 'rollup-plugin-uglify';
+
+const isDev = process.env.NODE_ENV === 'development';
+const minify = process.env.MINIFY;
+
+const config = {
+  input: 'src/snow.js',
+  output: {
+    file: 'src/snow.js',
+    format: 'umd',
+    name: '$snow'
+  }
+};
+
+if (isDev) {
+  
+} else {
+  if (minify === 'true') {
+    config.output.file = 'release/snow.min.js';
+    config.plugins.push(uglify.uglify());
+  } else {
+    config.output.file = 'release/show.js';
+  }
+}
+
+export default config;
